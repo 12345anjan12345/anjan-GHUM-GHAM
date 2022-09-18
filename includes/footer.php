ef40dc29194fc8acf8dc94819ff3a06f75f22127
12345anjan12345 <84557654+12345anjan12345@users.noreply.github.com>
2022-09-18 22:30:28 +0545

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/pratik.ghimire.50/"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://twitter.com/AnjanGh57266829"><span>Twitter</span></a></li>
+				<li><a class="flickr" href="https://www.flickr.com/photos/tags/login/"><span>Flickr</span></a></li>
+				<li><a class="googleplus" href="https://myaccount.google.com/profile"><span>Google+</span></a></li>
+				<li><a class="dribbble" href="https://dribbble.com/"><span>Dribbble</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2022 GHUM^GHAM . All Rights Reserved </p>
+	</div>
+</div>
